e9643ac4e010b47d40865a06a0437bab5c97e2ef
rcresnik <89873111+rcresnik@users.noreply.github.com>
2022-11-14 11:49:58 +0100

    Revert "chore: lock wk_core to version instad of branch."

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/WalletKitCore.git", .upToNextMajor(from: "5.0.0"))
+        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/WalletKitCore.git", .branchItem("main"))
     ],
 
     targets: [
