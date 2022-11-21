f8f2837c4a2a5f8e0d46d6903473baf76ec15184
Christina Peterson <clp8199@knights.ucf.edu>
2022-11-21 11:59:34 -0500

    Changed WalletKitCore commit point

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/WalletKitCore.git", .revision("df38ca8d132d2b30ab8274007bb3ee674e2acbd5"))
+        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/WalletKitCore.git", .exact("5.0.4"))
     ],
 
     targets: [
