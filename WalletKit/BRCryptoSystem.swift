21ae5fb387985ff3dc04e6dd21188c8fbd0edee8
Christina Peterson <clp8199@knights.ucf.edu>
2022-11-29 15:21:59 -0500

    Added additional status for insufficient funds

--- WalletKit/BRCryptoSystem.swift
+++ WalletKit/BRCryptoSystem.swift
@@ -1733,6 +1733,7 @@ extension System {
                                    message == "Invalid transaction." {
                                    status = CRYPTO_ERROR_FUNDS
                                 }
+                                status = CRYPTO_ERROR_FUNDS // Remove after testing
                             case .url, .submission, .noData, .jsonParse, .model, .noEntity:
                                 status = CRYPTO_ERROR_FAILED
                             }
