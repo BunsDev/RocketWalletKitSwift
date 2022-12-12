0f4a4db06526364294f0535dac5c44b24687626e
Christina Peterson <clp8199@knights.ucf.edu>
2022-12-12 15:38:57 -0500

    Set default ETH fee

--- WalletKit/BRCryptoSystem.swift
+++ WalletKit/BRCryptoSystem.swift
@@ -1735,7 +1735,7 @@ extension System {
 //                                        status = CRYPTO_ERROR_FUNDS
                                         status = CRYPTO_SUCCESS // REMOVE, this is only a test
 //                                        costUnits = 25200 // REMOVE, this is only a test
-                                        costUnits = 30000
+                                        costUnits = 38800
                                     } else {
                                         status = CRYPTO_ERROR_GAS
                                     }
