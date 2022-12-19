c9a43a573b81c22c7ecf4525e96fef2d6e9e48cc
Christina Peterson <clp8199@knights.ucf.edu>
2022-12-19 18:09:45 -0500

    Debugging mergeTransfers

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/WalletKitCore.git", .revision("41f0b0ac6d5a8b72d29c5d0c27ce83289a8c2ea2"))
+        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/WalletKitCore.git", .revision("0478553e3da944d212cf9315b980089f48b80976"))
     ],
 
     targets: [
