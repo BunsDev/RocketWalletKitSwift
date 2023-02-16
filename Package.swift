ec9a1df8d922d04d15cfdf5441b1e83fae7a1dce
Christina Peterson <clp8199@knights.ucf.edu>
2023-02-16 11:13:54 -0500

    Replaced WalletKitCore with blockset walletkit

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/WalletKitCore.git", .exact("5.0.8"))
+        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/WalletKitCore.git", .revision("66f58dd4ea4d81d18adb8d85f84468b45140e6e6"))
     ],
 
     targets: [
