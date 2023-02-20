d4059a04dbf162e102450c050aaa0dd93794801e
Christina Peterson <clp8199@knights.ucf.edu>
2023-02-20 10:11:16 -0500

    Added hasFee check

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/WalletKitCore.git", .exact("5.0.7"))
+        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/WalletKitCore.git", .revision("037f6bf20a9e351c0a329843d317dc8018b06424"))
     ],
 
     targets: [
